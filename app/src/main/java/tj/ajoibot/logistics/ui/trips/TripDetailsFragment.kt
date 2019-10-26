69e2b7e330de3e60c05ed4967b4d5b74797677d1
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-10-26 19:54:20 +0300

    nav to unload car fragment

--- app/src/main/java/tj/ajoibot/logistics/ui/trips/TripDetailsFragment.kt
+++ app/src/main/java/tj/ajoibot/logistics/ui/trips/TripDetailsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_trip_details.*
 import tj.ajoibot.logistics.R
 
 class TripDetailsFragment : BaseTripFragment() {
@@ -20,6 +22,19 @@ class TripDetailsFragment : BaseTripFragment() {
         textView.text = vm.selectedTrip?.id
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+    }
+
+    private fun setListeners(){
+        trip_details_unload_btn.setOnClickListener {
+            val action = TripDetailsFragmentDirections.navigateToUnloadCarFragment()
+            findNavController().navigate(action)
+        }
     }
 
     companion object {
