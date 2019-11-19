257bb9803959376a7f30b9f668ed06639d065baf
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2019-11-19 22:35:21 +0300

    Duob app launcher icon

--- app/src/main/java/tj/ajoibot/logistics/internal/Constants.kt
+++ app/src/main/java/tj/ajoibot/logistics/internal/Constants.kt
@@ -1,6 +1,6 @@
 package tj.ajoibot.logistics.internal
 
-const val BASE_URL = "http://10.0.2.2:8000/"
-const val BASE_URL_API = "http://10.0.2.2:8000/api/"
-//const val BASE_URL = "http://duob.ajoibot.tj/"
-//const val BASE_URL_API = "http://duob.ajoibot.tj/api/"
+//const val BASE_URL = "http://10.0.2.2:8000/"
+//const val BASE_URL_API = "http://10.0.2.2:8000/api/"
+const val BASE_URL = "http://duob.ajoibot.tj/"
+const val BASE_URL_API = "http://duob.ajoibot.tj/api/"
